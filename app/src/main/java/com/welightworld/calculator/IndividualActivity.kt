75559122d74553bba31b2c51f24457b1a561eb9f
何悦 <vipheyue@foxmail.com>
2018-04-10 18:51:43 +0800

    ...

---
+++ app/src/main/java/com/welightworld/calculator/IndividualActivity.kt
@@ -0,0 +1,12 @@
+package com.welightworld.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IndividualActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual)
+    }
+}
